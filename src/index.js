c839e17a973b076474d88b79353209623102e84c
Ramy Nassef <ramy.nassef@pwc.com>
2023-02-16 15:01:29 +0200

    Add React App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./component/App";
 import "./index.css";
-import "github-fork-ribbon-css/gh-fork-ribbon.css";
+import App from "./components/App/App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("root"));
