912e22dd66e9f07dcd013286c685a004a55e3df1
Ramy Nassef <ramy.nassef@pwc.com>
2023-02-16 13:19:57 +0200

    Add React App

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
-Emoji Search
----
+# A simple counter-App using [React.js](https://reactjs.org)
 
-Created with *create-react-app*. See the [full create-react-app guide](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
+> **This tiny application is like adding,removing,deleting, resetting products which reflects in the total number of products into our shopping cart.**
 
+**This is a very basic app but useful to understand the React Components, states, Data Flow, parent to child etc. .**
 
+## Live URL :-
 
-Install
----
+> [Counter App](https://obscure-waters-60500.herokuapp.com)
 
-`npm install`
+**OR your can do the following to run in your local machine**
 
+## Scripts
 
+Install node modules
 
-Usage
----
+#### `npm install`
 
-`npm start`
+In the project directory, you can run:
+
+#### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
--- src/App.js
+++ src/App.js
@@ -1,30 +1,73 @@
-import React, { PureComponent } from "react";
-import Header from "./Header";
-import SearchInput from "./SearchInput";
-import EmojiResults from "./EmojiResults";
-import filterEmoji from "./filterEmoji";
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
 
-export default class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filteredEmoji: filterEmoji("", 20)
-    };
-  }
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
 
-  handleSearchChange = event => {
-    this.setState({
-      filteredEmoji: filterEmoji(event.target.value, 20)
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counters[index] };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counters[index] };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
     });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleRestart = () => {
+    window.location.reload();
   };
 
   render() {
     return (
-      <div>
-        <Header />
-        <SearchInput textChange={this.handleSearchChange} />
-        <EmojiResults emojiData={this.state.filteredEmoji} />
+      <div className="main__wrap">
+        <main className="container">
+          <div className="card__box">
+            <NavBar
+              totalCounters={
+                this.state.counters.filter((c) => c.value > 0).length
+              }
+            />
+            <Counters
+              counters={this.state.counters}
+              onReset={this.handleReset}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
+              onRestart={this.handleRestart}
+            />
+          </div>
+        </main>
       </div>
     );
   }
 }
+
+export default App;
